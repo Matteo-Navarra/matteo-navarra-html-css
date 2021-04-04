6c3853d92b63eb6652a928b18b32a577ce4c4208
Matteo <matteonavarralifestyle@gmail.com>
2021-04-04 14:45:38 +0200

    migliorata grafica e responsive

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+function sendMail(params) {
+    var tempParams = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
+    };
+    emailjs.send('service_zmlxzyv', 'template_vlnrg8n', tempParams)
+        .then(function(res) {
+            console.log('succes', res.status);
+        })
+
+}
